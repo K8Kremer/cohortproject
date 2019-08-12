d5858b7221783d57907177e49f84aefad428b748
Kate Kremer <katrinakremer@gmail.com>
2019-08-12 16:53:11 -0400

    add top page header

--- server/client/src/components/admin/AdminView.js
+++ server/client/src/components/admin/AdminView.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import PageHeader from '../nav/navpane'
 
 const AdminView = () => {
   return (
+   
     <div>
+      <PageHeader />
       adminview - nav + routes
     </div>
   )
