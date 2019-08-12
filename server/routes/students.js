b6fa332e232c40d35cd26f07951d42bd26c58ea5
Jake Galligan <galligan1996@gmail.com>
2019-08-12 19:07:36 -0400

    Adding the ability to GET students with optional query to filter by cohort

--- server/routes/students.js
+++ server/routes/students.js
@@ -1,1 +1,34 @@
 const router = require('express').Router();
+const Student = require('../models/student');
+
+
+//set route to get students 
+router.get('/', (req,res,next) => {
+    let cohortQuery = req.query.cohort;
+    //if cohort query is specified return all students ordered by cohort
+    if (!cohortQuery) {
+        Student
+            .find()
+            .sort({'cohort': 1, 'name.last': 1})
+            .exec((err, students) => {
+                if (err) {
+                    res.status(400).send('Unable to retrieve students');
+                }
+                res.send(students);
+            })
+    //if there is a cohort query get students sorted by lastname
+    } else if (cohortQuery) {
+        Student
+            .find({cohort: cohortQuery})
+            .sort({'name.last': 1})
+            .exec((err, students) => {
+                if (err) {
+                    res.status(400).send('Make sure cohort query represents a valid cohort');                    
+                }
+                res.send(students);
+            })
+    }
+})
+
+
+module.exports = router;
