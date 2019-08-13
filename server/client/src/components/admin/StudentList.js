4d12c4c460d39476a05904ea8584374e6322b209
dposse <dposse.125@gmail.com>
2019-08-13 13:31:39 -0400

    Added get request to StudentList, needs fixing in redux

--- server/client/src/components/admin/StudentList.js
+++ server/client/src/components/admin/StudentList.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchStudents } from '../../actions';
 
-const StudentList = () => {
-  return (
-    <div>studentlist</div>
-  )
+class StudentList extends Component {
+  componentDidMount() {
+    this.props.fetchStudents();
+  }
+
+  render() {
+    return (
+      <div>studentlist</div>
+    )
+  }
 };
 
-export default StudentList;
+function mapStateToProps(state) {
+  return {
+    students: state.students
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchStudents }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(StudentList);
