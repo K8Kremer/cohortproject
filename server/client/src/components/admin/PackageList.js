b86b3d33b18bd8405038ff0d36da8aa2b4ea8105
snpdolan <snpdolan@gmail.com>
2019-08-13 14:04:24 -0400

    updated actions for fetchPackages, added in packageReducer to our Redux store, created PackageList front end component as a class-based component to view our sweet Packages.

--- server/client/src/components/admin/PackageList.js
+++ server/client/src/components/admin/PackageList.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { fetchPackages } from '../../actions';
+import { bindActionCreators } from 'redux';
+import {Link} from 'react-router-dom';
 
-const PackageList = () => {
-  return (
-    <div>PackageList</div>
-  )
+class PackageList extends Component {
+
+  componentDidMount = () => {
+    super();
+    this.props.fetchPackages();
+    this.wrapPackages = this.wrapPackages.bind(this);
+  }
+
+  wrapPackages = (packagesObject) => {
+    return packagesObject.map(package => {
+      return (
+        <li>
+          <Link to='/packages/{package._id}'> {package.packageName}</Link> 
+        </li>
+      )
+    })
+  }
+
+  render(){
+    if(packages == {}){
+      return(
+        <div>
+          No Packages here just yet! Please make one from our friendly action bar above.
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        <ul>
+          {this.wrapPackages(packages)}
+        </ul>
+      </div>
+    )
+  }
 };
 
-export default PackageList;
+
+function mapStateToProps(state) {
+  return {
+    packages: state.packages,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPackages }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PackageList);
