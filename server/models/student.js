b9608434b47bc77faa2894ff2d8cca0d22236663
Kyle Kimber <kimber.kyle@gmail.com>
2019-08-14 11:16:33 -0400

    Reformatted the Schemas so we do not have nested objects as per the request of the front end team

--- server/models/student.js
+++ server/models/student.js
@@ -2,24 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const StudentSchema = new Schema({
-  name: {
-    title: { type: String, required: True},
-    first: { type: String, required: True},
-    last: { type: String, required: True},  },
-  projectRepo = {
-    link: String,
-    description:  String 
-  },
+
+  firstName: { type: String, required: True},
+  lastName: { type: String, required: True},
+  projectRepoLink: String,
+  projectRepoDescription:  String,
   packages: [{type: Schema.Types.ObjectId, ref: 'Package'}],
   jobSeekingStatus: { type: String, required: True},
   employmentLocationPreference: { type: String, required: True},
   typeOfWorkDesired: { type: String, required: True},
   industriesPreferred: [],
-  picture: {
-    large: { type: String, required: True},
-    medium: { type: String, required: True},
-    thumbnail: { type: String, required: True}, 
-  },
+  picture: { type: String, required: True}, 
   bio: { type: String, required: True},
   address: { type: String, required: True},
   email: { type: String, required: True},
