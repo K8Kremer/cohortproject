50e4b4e5e03d5e34931e66f92017b7565a839316
Kate Kremer <katrinakremer@gmail.com>
2019-08-15 13:00:09 -0400

    fix styling and add create package button to link to correct page

--- server/client/src/components/admin/PackageList.js
+++ server/client/src/components/admin/PackageList.js
@@ -10,27 +10,37 @@ class PackageList extends Component {
   constructor(props){
     super(props);
     this.wrapPackages = this.wrapPackages.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   componentDidMount() {
     
     this.props.fetchPackages();
   }
 
+  handleClick(){
+    this.props.history.push(`/admin/createpackage`)
+  }
+
   wrapPackages(packagesArray){
     // console.log(packagesArray);
     
     return (
-      <>
-      <div className='row'style={{marginTop:78}}>
+      
+      <div className='row'>
       <div className='col-2'style={{backgroundColor:'#9EAEB8'}}>
 
       </div>
-      <div className='col-8'>
-        <div className='row'>
-      <h1>Packages</h1>
-      <button className='btn btn-primary' id='createPackage'>New Package</button>
-      </div>
-      <table className='shadow p-3 mb-5 bg-white rounded'style={{tableLayout: 'fixed'}}className='table table-hover'>
+      <div className='col-8' id='nopad'>
+       
+     <table className='shadow p-3 mb-5 bg-white rounded'style={{tableLayout: 'fixed'}}className='table table-hover'>
+       <tr style={{backgroundColor:'#9EAEB8'}}>
+       <th style={{width:'20%'}}><h1 className='styled-header'>Packages</h1></th>
+       <th style={{width:'20%'}}></th>
+          <th style={{width:'10%'}}></th>
+          <th style={{width:'20%'}}></th>
+          <th style={{width:'10%'}}><button className='btn btn-primary' id='createPackage' onClick={this.handleClick}>New Package</button></th>
+          <th style={{width:'10%'}}></th>
+       </tr>
         <tr style={{backgroundColor:'#679AB8'}}>
           <th style={{width:'20%'}}>Package</th>
           <th style={{width:'20%'}}>Recipient</th>
@@ -51,7 +61,7 @@ class PackageList extends Component {
       <div className='col-2'style={{backgroundColor:'#9EAEB8'}}> </div>
       
       </div>
-      </>
+    
     );
   }
     
@@ -67,9 +77,7 @@ class PackageList extends Component {
 
     return (
       <div>
-        <ul>
           {this.wrapPackages(this.props.packages)}
-        </ul>
       </div>
     )
   }
