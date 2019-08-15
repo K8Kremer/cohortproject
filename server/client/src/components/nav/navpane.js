ed1004fb0a623fc09faca82f2e2f9fed97cb3e6a
Kate Kremer <katrinakremer@gmail.com>
2019-08-14 23:10:52 -0400

    fix side menu and page header

--- server/client/src/components/nav/navpane.js
+++ server/client/src/components/nav/navpane.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import './navstyles.css'
 
 function PageHeader () {
 return(
 
-<nav className="navbar navbar-light" style={{backgroundColor: "#d2d2d4", height: 75}}>
+<nav className="navbar navbar-light" id='sticky' >
   <a className="nabar-brand" href="#">
     <img style={{marginTop: 15, marginLeft: 75}}src="https://www.projectshift.io/wp-content/uploads/2018/09/Asset-5@200x-3.png" width="200" ></img>
   </a>
