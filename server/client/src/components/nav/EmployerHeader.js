9dccf8779b8f775e38622c45599ecfd90dc9fa7a
Ronnie <rscott910@outlook.com>
2019-08-15 18:58:26 -0400

    Add functionality to the nav header, for now until we can pass in actual ids

--- server/client/src/components/nav/EmployerHeader.js
+++ server/client/src/components/nav/EmployerHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const EmployerHeader = () => (
   <nav className="navbar navbar-light" style={{backgroundColor: "#ffffff", height: 75}}>
-    <a className="navbar-brand" href="#">
+    <a className="navbar-brand" href="/employer/:packageId">
       <img style={{marginLeft: 15, verticalAlign: 'baseline' }} 
       src="https://www.projectshift.io/wp-content/uploads/2018/09/Asset-5@200x-3.png" 
       height="49px" 
--- server/client/src/components/nav/navpane.js
+++ server/client/src/components/nav/navpane.js
@@ -5,7 +5,7 @@ function PageHeader () {
 return(
 
 <nav className="navbar navbar-light" id='sticky' >
-  <a className="nabar-brand" href="#">
+  <a className="nabar-brand" href="/">
     <img style={{marginLeft: 10, verticalAlign: 'baseline' }}src="https://www.projectshift.io/wp-content/uploads/2018/09/Asset-5@200x-3.png" width="200" height="49px" 
       className="d-inline-block align-top" ></img>
   </a>
