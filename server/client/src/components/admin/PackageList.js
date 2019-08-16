eb92f30e25e795da648b0745f89b6b327e43941c
unknown <nathaniel.poli@gmail.com>
2019-08-16 11:16:52 -0400

    Added filter by status to package list view

--- server/client/src/components/admin/PackageList.js
+++ server/client/src/components/admin/PackageList.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { Button } from 'react-bootstrap';
+import { Button, Dropdown } from 'react-bootstrap';
 import { fetchPackages } from '../../actions';
 import { bindActionCreators } from 'redux';
 import PackageRow from './PackageRow';
@@ -14,9 +14,22 @@ class PackageList extends Component {
     this.wrapPackages = this.wrapPackages.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
+
+  state = {
+    filterChoice : '',
+    filterName : ''
+  }
   componentDidMount() {
     
     this.props.fetchPackages();
+  }
+
+  async componentDidUpdate(prevProps, prevState) {
+    let prevChoice = await prevState.filterChoice
+    let currChoice = await this.state.filterChoice
+    if (prevChoice !== currChoice) {
+
+    }
   }
 
   handleClick(){
@@ -24,40 +37,6 @@ class PackageList extends Component {
   }
 
   wrapPackages(packagesArray){
-    console.log(packagesArray)
-    if (packagesArray == "There are no packages with that status") {
-      return (
-        <div className='row mx-0 pt-3 pb-3' style={{backgroundColor:'#9EAEB8', height: '100%', minHeight: '100vh'}}>
-        <div className='mx-3 px-3' style={{backgroundColor:'#FFFFFF', width: '100%'}}>
-       
-
-          <div className ='d-flex justify-content-between flex-row bd-highlight mb-3 mt-3'>
-
-            <h3>No Matching Packages</h3>
-            <Button className='create-package' style={{backgroundColor: '#679AB8', borderColor: '#679AB8'}}
-              onClick={e=> this.props.history.push('/admin/createpackage')}>New Package</Button>
-          </div>
-
-          <div className ='d-flex justify-content-between flex-row bd-highlight mb-3 mt-3'>    
-          <SearchBar searchType='packages' dropdownFilter='true'/>
-          </div>
-        
-        <table className='shadow p-3 mb-5 bg-white rounded'style={{tableLayout: 'fixed'}}className='table table-hover'>
-            <tbody>
-              <tr style={{backgroundColor:'#679AB8', color: '#ffffff'}}>
-                <th style={{width:'20%'}}>Package</th>
-                <th style={{width:'20%'}}>Recipient</th>
-                <th style={{width:'20%'}}>Status</th>
-                <th style={{width:'20%'}}>Date Created</th>
-                <th style={{ textAlign: 'center',width:'20%'}}></th>
-                {/* <th style={{textAlign: 'center', width:'10%'}}></th> */}
-              </tr>
-          </tbody>
-        </table>
-        </div> 
-        </div>)
-    }
-    
     return (
       
       <div className='row mx-0 pt-3 pb-3' style={{backgroundColor:'#9EAEB8', height: '100%', minHeight: '100vh'}}>
@@ -71,8 +50,27 @@ class PackageList extends Component {
               onClick={e=> this.props.history.push('/admin/createpackage')}>New Package</Button>
           </div>
 
-          <div className ='d-flex justify-content-between flex-row bd-highlight mb-3 mt-3'>    
-          <SearchBar searchType='packages' dropdownFilter='true'/>
+          <div className ='d-flex justify-content-between flex-row bd-highlight mb-3 mt-3'>
+            {/* Currently to get the search to work properly after you filter you need to start typing in the search bar*/}
+          <Dropdown>
+              <span>Filter by Status: </span>
+              <Dropdown.Toggle variant="primary" id="dropdown-basic" style={{backgroundColor: '#679AB8', borderColor: '#679AB8'}}>
+                 {this.state.filterName || 'filters'}
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  onClick={e=> this.setState({filterChoice: 'true', filterName: 'Viewed'})}
+                >Viewed</Dropdown.Item>
+                <Dropdown.Item
+                  onClick={e=> this.setState({filterChoice: 'false', filterName: 'Unopened'})}
+                >Unopened</Dropdown.Item>
+                <Dropdown.Item
+                  onClick={e=> this.setState({filterChoice: null, filterName: 'All'})}
+                >All</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>   
+            <SearchBar searchType='packages' dropdownFilter={this.state.filterChoice} placeHolder='search'/>
           </div>
 
           <table className='shadow p-3 mb-5 bg-white rounded'style={{tableLayout: 'fixed'}}className='table table-hover'>
